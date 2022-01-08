5bb7e451643fc5a6ab14dc58b4c7cfd2e4c33403
Amanda Gnecco <54418920+amandagnecco@users.noreply.github.com>
2022-01-07 21:12:47 -0300

    Add files via upload

---
+++ universo.py
@@ -0,0 +1,9 @@
+for n in range (1, 101):
+    if n % 3 == 0 and n % 5 == 0:
+        print("Universo")
+    elif n % 3 == 0:
+        print("Uni")
+    elif n % 5 ==0:
+        print("Verso")
+    else:
+        print(n)
